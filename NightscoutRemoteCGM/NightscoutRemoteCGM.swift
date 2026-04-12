14c7dc9e5955a6d0416b75165fb9fcce7fac4ae6
Tim <timothy.theis@gmail.com>
2026-04-12 13:12:12 -0400

    Update NightscoutRemoteCGM.swift

--- NightscoutRemoteCGM/NightscoutRemoteCGM.swift
+++ NightscoutRemoteCGM/NightscoutRemoteCGM.swift
@@ -1,92 +1,216 @@
-import Foundation
-import HealthKit
+//
+//  NightscoutAPIManager.swift
+//  NightscoutRemoteCGM
+//
+//  Created by Ivan Valkou on 10.10.2019.
+//  Copyright © 2019 Ivan Valkou. All rights reserved.
+//
+
 import LoopKit
-import UserNotifications
+import HealthKit
 import Combine
 import NightscoutKit
 import CryptoKit
 
 public class NightscoutRemoteCGM: CGMManager {
+    
     public static let pluginIdentifier = "NightscoutRemoteCGM"
-    public static let localizedTitle = LocalizedString("Nightscout Remote CGM", comment: "Title")
-    public var localizedTitle: String { return NightscoutRemoteCGM.localizedTitle }
+    
+    public static let localizedTitle = LocalizedString("Nightscout Remote CGM", comment: "Title for the CGMManager option")
+    
+    public var localizedTitle: String {
+        return NightscoutRemoteCGM.localizedTitle
+    }
+    
     public var glucoseDisplay: GlucoseDisplayable? { latestBackfill }
-    public var cgmManagerStatus: CGMManagerStatus { .init(hasValidSensorSession: isOnboarded, device: device) }
-    public var isOnboarded: Bool { keychain.getNightscoutCgmURL() != nil }
-    public enum CGMError: String, Error { case tooFlatData = "BG data is too flat." }
+    
+    public var cgmManagerStatus: CGMManagerStatus {
+        return .init(hasValidSensorSession: isOnboarded, device: device)
+    }
+    
+    public var isOnboarded: Bool {
+        return keychain.getNightscoutCgmURL() != nil
+    }
+    
+    public enum CGMError: String, Error {
+        case tooFlatData = "BG data is too flat."
+    }
+
+    private enum Config {
+        static let useFilterKey = "NightscoutRemoteCGM.useFilter"
+        static let filterNoise = 2.5
+    }
 
     public init() {
         nightscoutService = NightscoutAPIService(keychainManager: keychain)
-        updateTimer = DispatchTimer(timeInterval: 60, queue: processQueue)
+        updateTimer = DispatchTimer(timeInterval: 10, queue: processQueue)
         scheduleUpdateTimer()
     }
 
-    public convenience required init?(rawState: CGMManager.RawStateValue) { self.init() }
-
-    public var rawState: CGMManager.RawStateValue { [:] }
-    private let keychain = KeychainManager()
-    public var nightscoutService: NightscoutAPIService {
-        didSet { keychain.setNightscoutCgmCredentials(nightscoutService.url, apiSecret: nightscoutService.apiSecret) }
+    public convenience required init?(rawState: CGMManager.RawStateValue) {
+        self.init()
+        useFilter = rawState[Config.useFilterKey] as? Bool ?? false
     }
+
+    public var rawState: CGMManager.RawStateValue {
+        [
+            Config.useFilterKey: useFilter
+        ]
+    }
+
+    private let keychain = KeychainManager()
+
+    public var nightscoutService: NightscoutAPIService {
+        didSet {
+            keychain.setNightscoutCgmCredentials(nightscoutService.url, apiSecret: nightscoutService.apiSecret)
+        }
+    }
+
     public let delegate = WeakSynchronizedDelegate<CGMManagerDelegate>()
-    public var delegateQueue: DispatchQueue! { get { delegate.queue } set { delegate.queue = newValue } }
-    public var cgmManagerDelegate: CGMManagerDelegate? { get { delegate.delegate } set { delegate.delegate = newValue } }
+
+    public var delegateQueue: DispatchQueue! {
+        get { delegate.queue }
+        set { delegate.queue = newValue }
+    }
+
+    public var cgmManagerDelegate: CGMManagerDelegate? {
+        get { delegate.delegate }
+        set { delegate.delegate = newValue }
+    }
+
     public let providesBLEHeartbeat = false
+
     public var managedDataInterval: TimeInterval?
+
     public var shouldSyncToRemoteService = false
+
     public var useFilter = false
+
     public private(set) var latestBackfill: GlucoseEntry?
+
+    private var requestReceiver: Cancellable?
+
     private let processQueue = DispatchQueue(label: "NightscoutRemoteCGM.processQueue")
+
     private var isFetching = false
 
     public func fetchNewDataIfNeeded(_ completion: @escaping (CGMReadingResult) -> Void) {
-        guard !isFetching else { completion(.noData); return }
-        
-        processQueue.async {
-            self.isFetching = true
-            
-            // --- TRAFFIC CONTROLLER ---
-            if self.keychain.getUseDirectLibre() {
+        // --- TRAFFIC CONTROLLER ---
+        if UserDefaults.standard.string(forKey: "com.loopkit.NightscoutRemoteCGM.UseDirectLibre") == "true" {
+            guard !isFetching else {
+                delegateQueue.async { completion(.noData) }
+                return
+            }
+            processQueue.async {
+                self.isFetching = true
                 self.fetchLibreData { result in
                     self.isFetching = false
                     self.delegateQueue.async { completion(result) }
                 }
-            } else {
-                self.fetchNightscoutData { result in
+            }
+        } else {
+            // --- ORIGINAL NIGHTSCOUT LOGIC ---
+            guard let nightscoutClient = nightscoutService.client, !isFetching else {
+                delegateQueue.async {
+                    completion(.noData)
+                }
+                return
+            }
+
+            if let latestGlucose = latestBackfill, latestGlucose.startDate.timeIntervalSinceNow > -TimeInterval(minutes: 4.5) {
+                delegateQueue.async {
+                    completion(.noData)
+                }
+                return
+            }
+
+            processQueue.async {
+                self.isFetching = true
+
+                nightscoutClient.fetchRecent { fetchResult in
+                    
                     self.isFetching = false
-                    self.delegateQueue.async { completion(result) }
+                    
+                    switch fetchResult {
+                    case .success(let glucoseEntries):
+                        guard !glucoseEntries.isEmpty else {
+                            self.delegateQueue.async {
+                                completion(.noData)
+                            }
+                            return
+                        }
+
+                        var filteredGlucose = glucoseEntries
+                        if self.useFilter {
+                            var filter = KalmanFilter(stateEstimatePrior: glucoseEntries.last!.glucose, errorCovariancePrior: Config.filterNoise)
+                            filteredGlucose.removeAll()
+                            for item in glucoseEntries.reversed() {
+                                let prediction = filter.predict(stateTransitionModel: 1, controlInputModel: 0, controlVector: 0, covarianceOfProcessNoise: Config.filterNoise)
+                                let update = prediction.update(measurement: item.glucose, observationModel: 1, covarienceOfObservationNoise: Config.filterNoise)
+                                filter = update
+                                filteredGlucose.append(
+                                    GlucoseEntry(
+                                        glucose: filter.stateEstimatePrior.rounded(),
+                                        date: item.date,
+                                        device: item.device,
+                                        glucoseType: item.glucoseType,
+                                        trend: item.trend,
+                                        changeRate: item.changeRate,
+                                        id: item.id
+                                    )
+                                )
+                            }
+                            filteredGlucose = filteredGlucose.reversed()
+                        }
+
+                        let startDate = self.delegate.call { (delegate) -> Date? in
+                            return delegate?.startDateToFilterNewData(for: self)
+                        }
+                        let newGlucose = filteredGlucose.filterDateRange(startDate, nil)
+                        let newSamples = newGlucose.filter({ $0.isStateValid }).map { glucose -> NewGlucoseSample in
+                            let glucoseTrend: LoopKit.GlucoseTrend?
+                            if let trend = glucose.trend {
+                                glucoseTrend = LoopKit.GlucoseTrend(rawValue: trend.rawValue)
+                            } else {
+                                glucoseTrend = nil
+                            }
+                            return NewGlucoseSample(
+                                date: glucose.startDate,
+                                quantity: HKQuantity(unit: .milligramsPerDeciliter, doubleValue: glucose.glucose),
+                                condition: nil,
+                                trend: glucoseTrend,
+                                trendRate: glucose.trendRate,
+                                isDisplayOnly: glucose.isCalibration == true,
+                                wasUserEntered: glucose.glucoseType == .meter,
+                                syncIdentifier: glucose.id ?? "\(Int(glucose.startDate.timeIntervalSince1970))",
+                                device: self.device)
+                        }
+
+                        if let latestBackfill = newGlucose.max(by: {$0.startDate > $1.startDate}) {
+                            self.latestBackfill = latestBackfill
+                        }
+
+                        self.delegateQueue.async {
+                            guard !newSamples.isEmpty else {
+                                completion(.noData)
+                                return
+                            }
+                            completion(.newData(newSamples))
+                        }
+                    case let .failure(error):
+                        self.delegateQueue.async {
+                            completion(.error(error))
+                        }
+                    }
                 }
             }
         }
     }
 
-    // --- RESTORED NIGHTSCOUT SIDE ---
-    private func fetchNightscoutData(_ completion: @escaping (CGMReadingResult) -> Void) {
-        guard let url = nightscoutService.url else { completion(.noData); return }
-        
-        // Fixed: Swapped to the correct NightscoutFetcher class
-        let fetcher = NightscoutFetcher(url: url, apiSecret: nightscoutService.apiSecret ?? "")
-        fetcher.fetchGlucose(since: Date().addingTimeInterval(.hours(-24))) { (result) in
-            switch result {
-            case .success(let entries):
-                let samples = entries.compactMap { entry -> NewGlucoseSample? in
-                    guard let quantity = entry.glucoseQuantity else { return nil }
-                    return NewGlucoseSample(date: entry.date, quantity: quantity, condition: nil, trend: nil, trendRate: nil, isDisplayOnly: false, wasUserEntered: false, syncIdentifier: entry.id)
-                }
-                if let lastEntry = entries.last {
-                    self.latestBackfill = lastEntry
-                }
-                completion(.newData(samples))
-            case .failure:
-                completion(.noData)
-            }
-        }
-    }
-
-    // --- DIRECT LIBRE SIDE ---
+    // --- DIRECT LIBRE LOGIC ---
     private func fetchLibreData(_ completion: @escaping (CGMReadingResult) -> Void) {
-        let email = keychain.getLibreEmail() ?? ""
-        let pass = keychain.getLibrePassword() ?? ""
+        let email = UserDefaults.standard.string(forKey: "com.loopkit.NightscoutRemoteCGM.LibreEmail") ?? ""
+        let pass = UserDefaults.standard.string(forKey: "com.loopkit.NightscoutRemoteCGM.LibrePassword") ?? ""
         
         let authUrl = URL(string: "https://api-us.libreview.io/llu/auth/login")!
         var authReq = URLRequest(url: authUrl)
@@ -207,40 +331,49 @@ public class NightscoutRemoteCGM: CGMManager {
     }
 
     public var device: HKDevice? = nil
-    public var debugDescription: String { "## NightscoutRemoteCGM\n" }
+
+    public var debugDescription: String {
+        "## NightscoutRemoteCGM\nlatestBackfill: \(String(describing: latestBackfill))\n"
+    }
+
+    public var appURL: URL? {
+        guard let url = nightscoutService.url else { return nil }
+        switch url.absoluteString {
+        case "http://127.0.0.1:1979":
+            return URL(string: "spikeapp://")
+        case "http://127.0.0.1:17580":
+            return URL(string: "diabox://")
+        default:
+            return url
+        }
+    }
+
     private let updateTimer: DispatchTimer
+
     private func scheduleUpdateTimer() {
         updateTimer.suspend()
         updateTimer.eventHandler = { [weak self] in
             guard let self = self else { return }
             self.fetchNewDataIfNeeded { result in
-                if case .newData = result { self.delegate.notify { $0?.cgmManager(self, hasNew: result) } }
+                guard case .newData = result else { return }
+                self.delegate.notify { delegate in
+                    delegate?.cgmManager(self, hasNew: result)
+                }
             }
         }
         updateTimer.resume()
     }
 }
 
-// --- KEYCHAIN EXTENSION ---
-// Fixed: Swapped strict keychain API calls to standard UserDefaults
-extension KeychainManager {
-    private enum LibreKey: String {
-        case useDirect = "com.loopkit.NightscoutRemoteCGM.UseDirectLibre"
-        case email = "com.loopkit.NightscoutRemoteCGM.LibreEmail"
-        case password = "com.loopkit.NightscoutRemoteCGM.LibrePassword"
+// MARK: - AlertResponder implementation
+extension NightscoutRemoteCGM {
+    public func acknowledgeAlert(alertIdentifier: Alert.AlertIdentifier, completion: @escaping (Error?) -> Void) {
+        completion(nil)
     }
-    func setUseDirectLibre(_ useDirect: Bool) { UserDefaults.standard.set(useDirect ? "true" : "false", forKey: LibreKey.useDirect.rawValue) }
-    func getUseDirectLibre() -> Bool { return UserDefaults.standard.string(forKey: LibreKey.useDirect.rawValue) == "true" }
-    func setLibreCredentials(email: String, pass: String) {
-        UserDefaults.standard.set(email, forKey: LibreKey.email.rawValue)
-        UserDefaults.standard.set(pass, forKey: LibreKey.password.rawValue)
-    }
-    func getLibreEmail() -> String? { return UserDefaults.standard.string(forKey: LibreKey.email.rawValue) }
-    func getLibrePassword() -> String? { return UserDefaults.standard.string(forKey: LibreKey.password.rawValue) }
 }
 
+// MARK: - AlertSoundVendor implementation
 extension NightscoutRemoteCGM {
-    public func acknowledgeAlert(alertIdentifier: Alert.AlertIdentifier, completion: @escaping (Error?) -> Void) { completion(nil) }
     public func getSoundBaseURL() -> URL? { return nil }
     public func getSounds() -> [Alert.Sound] { return [] }
 }
